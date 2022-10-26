08e44a0c08e19e3211bbc525c4529ef031ee9be3
diegoOG09 <xdiegoemmax@outlook.com>
2022-10-25 19:07:41 -0500

    daily coding problem-1 solution

---
+++ daily_problem/number_k.py
@@ -0,0 +1,17 @@
+numbers = [10,15,3,7]
+first_element = numbers[0]
+results = []
+
+def add_up(k):
+    for n in numbers:
+        for i in numbers:
+            add = n+i
+            results.append(add)
+    if k in results:
+        print(True)
+    else:
+        print(False)
+
+
+add_up(18)
+
